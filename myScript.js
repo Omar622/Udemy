61e1c19759b7996b02315804b8f290e55b43172b
Omar622 <omaramin622@gmail.com>
2022-08-14 17:48:57 +0300

    phase3: add carousel-bootstrap and manipulate it

--- myScript.js
+++ myScript.js
@@ -5,6 +5,7 @@ let courses = {}
 let titles = {}
 let headers = {}
 let descriptions = {}
+let number_of_carts = 4
 
 // build star rating design
 const build_stars = (rating) => {
@@ -32,74 +33,108 @@ const build_stars = (rating) => {
     return stars
 }
 
+const buildCart = (i, item) => {
+    let newImg = document.createElement("img")
+    newImg.src = item["image"]
+    newImg.alt = fields[i]
+    newImg.height = "150"
+    newImg.width = "260"
+
+    let CourseName = document.createElement("h3");
+    CourseName.style.cssText = "margin: 5px; font-size: 15px;"
+    let title = item["title"]
+    if(title.length > 41) title = title.substring(0, 40) + "..."
+    CourseName.append(title)
+
+    let instructorName = document.createElement("p")
+    instructorName.classList.add("coures-grid-item-description")
+    instructorName.style.cssText = "color: #876f89;"
+    let authors = "";
+    for(str of item["instructors"]) authors += str["name"] + ", "
+    authors = authors.substring(0, authors.length-2)
+    if(authors.length > 40){
+        authors = authors.substring(0, 39)
+        authors += "..."
+    }
+    instructorName.append(authors)
+
+    let rating = document.createElement("p")
+    rating.style.cssText = "display: inline; margin: 5px; font-size: 12px; color: #ce810e;"
+    rating.append(item["rating"] + " ")
+
+    let stars = build_stars(parseFloat(item["rating"]))
+
+    // adding random number of people
+    let retedby = document.createElement("p")
+    retedby.style.cssText = "display: inline; margin: 5px; font-size: 12px; color: #738abb;"
+    retedby.append(" (" + (Math.floor(Math.random()*10)+1) + "," + (Math.floor(Math.random()*900)+100) + ")")
+
+    let price = document.createElement("h3");
+    price.style.cssText = "margin: 5px; font-size: 18px; display: block;"
+    price.append("E€ " + item["price"])
+
+    let newHeader = document.createElement("header");
+    newHeader.style.cssText = "padding-left: 12px;"
+    newHeader.append(CourseName)
+    newHeader.appendChild(instructorName)
+    newHeader.appendChild(rating)
+    for(star of stars) newHeader.appendChild(star)
+    newHeader.appendChild(retedby)
+    newHeader.appendChild(price)
+
+    let newCart = document.createElement("article")
+    newCart.classList.add("courses-grid-item")
+    newCart.appendChild(newImg)
+    newCart.appendChild(newHeader)
+    return newCart
+}
+
 // show courses cards in field 'field' and they title must match with 'match'
 const showField = (i, match) => {
     document.getElementById('courses-field-header').textContent = headers[fields[i]]
     document.getElementById('courses-field-description').textContent = descriptions[fields[i]]
     document.getElementById('courses-field-button').textContent = "Explore " + fields[i]
 
-    let course_gird = document.querySelector('.courses-grid')
-    course_gird.innerHTML = '' // remove all its children
+    let div_carousel = document.getElementById('courses-grid-carousel-inner')
+    div_carousel.innerHTML = '' // remove all its children
+
+    let div_of_carts = document.createElement('div')
+    div_of_carts.classList.add('box-of-carts')
+    let carts_added = 0;
+    let flag_active = true;
     for(item of courses[fields[i]]){
+        // check first if the title match the 'match' (it is guaranteed that 'match' is lower case)
         let tmpTitle = item["title"]
         tmpTitle = tmpTitle.toLowerCase()
-        // check first if the title match the 'match' (it is guaranteed that 'match' is lower case)
         if(!tmpTitle.includes(match)) continue
         
-        let newImg = document.createElement("img")
-        newImg.src = item["image"]
-        newImg.alt = fields[i]
-        newImg.height = "150"
-        newImg.width = "260"
-
-        let CourseName = document.createElement("h3");
-        CourseName.style.cssText = "margin: 5px"
-        let title = item["title"]
-        if(title.length > 41) title = title.substring(0, 40) + "..."
-        CourseName.append(title)
-
-        let instructorName = document.createElement("p")
-        instructorName.classList.add("coures-grid-item-description")
-        instructorName.style.cssText = "color: #876f89;"
-        let authors = "";
-        for(str of item["instructors"]) authors += str["name"] + ", "
-        authors = authors.substring(0, authors.length-2)
-        if(authors.length > 40){
-            authors = authors.substring(0, 39)
-            authors += "..."
+        newCart = buildCart(i, item)
+        div_of_carts.appendChild(newCart)
+        ++carts_added
+        if(carts_added == number_of_carts){
+            carts_added = 0
+            let carousel_item = document.createElement('div');
+            carousel_item.classList.add('carousel-item')
+            if(flag_active){
+                carousel_item.classList.add("active")
+                flag_active = false
+            }
+            carousel_item.appendChild(div_of_carts)
+            div_of_carts = document.createElement('div')
+            div_of_carts.classList.add('box-of-carts')
+            div_carousel.appendChild(carousel_item)
         }
-        instructorName.append(authors)
-
-        let rating = document.createElement("p")
-        rating.style.cssText = "display: inline; margin: 5px; font-size: 12px; color: #ce810e;"
-        rating.append(item["rating"] + " ")
-
-        let stars = build_stars(parseFloat(item["rating"]))
-
-        // adding random number of people
-        let retedby = document.createElement("p")
-        retedby.style.cssText = "display: inline; margin: 5px; font-size: 12px; color: #738abb;"
-        retedby.append(" (" + (Math.floor(Math.random()*10)+1) + "," + (Math.floor(Math.random()*900)+100) + ")")
-
-        let price = document.createElement("h3");
-        price.style.cssText = "margin: 5px"
-        price.append("E€ " + item["price"])
-
-        let newHeader = document.createElement("header");
-        newHeader.style.cssText = "padding-left: 12px;"
-        newHeader.append(CourseName)
-        newHeader.appendChild(instructorName)
-        newHeader.appendChild(rating)
-        for(star of stars) newHeader.appendChild(star)
-        newHeader.appendChild(retedby)
-        newHeader.appendChild(price)
-
-        let newCart = document.createElement("section")
-        newCart.classList.add("courses-grid-item")
-        newCart.appendChild(newImg)
-        newCart.appendChild(newHeader)
-        
-        course_gird.appendChild(newCart)
+    }
+    if(carts_added !== 0){
+        carts_added = 0
+        let carousel_item = document.createElement('div');
+        carousel_item.classList.add('carousel-item')
+        if(flag_active){
+            carousel_item.classList.add("active")
+            flag_active = false
+        }
+        carousel_item.appendChild(div_of_carts)
+        div_carousel.appendChild(carousel_item)
     }
 }
 
@@ -129,7 +164,7 @@ function loadAllData(i = 0){
 
 
 loadAllData()
-
+// add events for tabs
 for(let i = 0; i < tabsIds.length; ++i){
     document.getElementById(tabsIds[i]).addEventListener("click", () => {
         field_index = i;
