4b6cc28b167835b6b127896d785a51c21c58e022
Omar622 <omaramin622@gmail.com>
2022-08-14 13:48:04 +0300

    phase3: make courses tabs work

--- myScript.js
+++ myScript.js
@@ -1,4 +1,10 @@
+const tabsIds = ['python-cat-button', 'Excel-cat-button', 'Web-cat-button', 'JavaScript-cat-button', 'Data-cat-button', 'AWS-cat-button', 'Drawing-cat-button']
+const fields = ['Python', 'Excel', 'Web-Development', 'JavaScript', 'Data-Science', 'AWS-Certification', 'Drawing']
+let field_index = 0;
 let courses = {}
+let titles = {}
+let headers = {}
+let descriptions = {}
 
 // build star rating design
 const build_stars = (rating) => {
@@ -6,20 +12,20 @@ const build_stars = (rating) => {
     let rem = 5;
     while(parseFloat(rating, 10) >= .7){
         let newIcon = document.createElement("span")
-        newIcon.innerHTML = '<i class="fa fa-star" style="font-size:12px;color:#ebb252"></i>'
+        newIcon.innerHTML = '<i class="fa fa-star" style="font-size:12px; margin: 1px; color:#ebb252"></i>'
         stars.push(newIcon)
         rating -= 1
         --rem;
     }
     if(parseFloat(rating, 10) >= 0.3){
         let newIcon = document.createElement("span")
-        newIcon.innerHTML = '<i class="fa fa-star-half-empty" style="font-size:12px;color:#ebb252"></i>'
+        newIcon.innerHTML = '<i class="fa fa-star-half-empty" style="font-size:12px; margin: 1px; color:#ebb252"></i>'
         stars.push(newIcon)
         --rem;
     }
     while(rem > 0){
         let newIcon = document.createElement("span")
-        newIcon.innerHTML = '<i class="fa fa-star-o" style="font-size:12px;color:#ebb252"></i>'
+        newIcon.innerHTML = '<i class="fa fa-star-o" style="font-size:12px; margin: 1px; color:#ebb252"></i>'
         stars.push(newIcon)
         --rem;
     }
@@ -34,8 +40,12 @@ const loadData = async (branch) => {
 }
 
 // show courses cards in field 'field' and they title must match with 'match'
-const showCourses = (field, match) => {
+const showField = (field, match) => {
     match = match.toLowerCase()
+    document.getElementById('courses-field-header').textContent = headers[field]
+    document.getElementById('courses-field-description').textContent = descriptions[field]
+    document.getElementById('courses-field-button').textContent = "Explore " + field
+
     let course_gird = document.querySelector('.courses-grid')
     course_gird.innerHTML = '' // remove all its children
     for(item of courses[field]){
@@ -46,34 +56,42 @@ const showCourses = (field, match) => {
         
         let newImg = document.createElement("img")
         newImg.src = item["image"]
-        newImg.alt = "python"
+        newImg.alt = field
         newImg.height = "150"
         newImg.width = "260"
 
         let CourseName = document.createElement("h3");
         CourseName.style.cssText = "margin: 5px"
         let title = item["title"]
-        if(title.length > 45) title = title.substring(0, 44) + "..."
+        if(title.length > 41) title = title.substring(0, 40) + "..."
         CourseName.append(title)
 
         let instructorName = document.createElement("p")
         instructorName.classList.add("coures-grid-item-description")
         instructorName.style.cssText = "color: #876f89;"
-        instructorName.append(item["author"])
+        let authors = "";
+        for(str of item["instructors"]) authors += str["name"] + ", "
+        authors = authors.substring(0, authors.length-2)
+        if(authors.length > 40){
+            authors = authors.substring(0, 39)
+            authors += "..."
+        }
+        instructorName.append(authors)
 
         let rating = document.createElement("p")
-        rating.style.cssText = "display: inline; font-size: 12px; color: #ce810e;"
+        rating.style.cssText = "display: inline; margin: 5px; font-size: 12px; color: #ce810e;"
         rating.append(item["rating"] + " ")
 
+        let stars = build_stars(parseFloat(item["rating"]))
+
+        // adding random number of people
         let retedby = document.createElement("p")
-        retedby.style.cssText = "display: inline; font-size: 12px; color: #738abb;"
-        retedby.append("(" + item["people"] + ")")
+        retedby.style.cssText = "display: inline; margin: 5px; font-size: 12px; color: #738abb;"
+        retedby.append(" (" + (Math.floor(Math.random()*10)+1) + "," + (Math.floor(Math.random()*900)+100) + ")")
 
         let price = document.createElement("h3");
         price.style.cssText = "margin: 5px"
         price.append("E€ " + item["price"])
-
-        stars = build_stars(parseFloat(item["rating"]))
 
         let newHeader = document.createElement("header");
         newHeader.style.cssText = "padding-left: 12px;"
@@ -93,20 +111,45 @@ const showCourses = (field, match) => {
     }
 }
 
-// load data
-loadData("python").then((res) => {
-    courses["python"] = res
-}).then(() => {
-    showCourses("python", "")
+// load all data
+function loadAllData(i = 0){
+    if(i === fields.length){
+        showField(fields[field_index], "");
+        return;
+    }
+    loadData(fields[i]).then((res) => {
+        courses[fields[i]] = res["courses"]
+        titles[fields[i]] = res["title"]
+        headers[fields[i]] = res["header"]
+        descriptions[fields[i]] = res["description"]
+    }).then(() => {
+        loadAllData(i+1)
+    })
+}
+
+
+
+loadAllData()
+
+for(let i = 0; i < tabsIds.length; ++i){
+    document.getElementById(tabsIds[i]).addEventListener("click", () => {
+        field_index = i;
+        showField(fields[field_index], "")
+        document.getElementById(tabsIds[i]).style.color = "black"
+        for(let j = 0; j < tabsIds.length; ++j){
+            if(i === j) continue
+            document.getElementById(tabsIds[j]).style.color = "#6a6f73"
+        }
+    })
+}
+
+// add event to search bar
+document.getElementById('search-submit').addEventListener("click", () => {
+    let text = document.getElementById('search-input').value;
+    showField(fields[field_index], text)
 })
 
 // stop form from reloading page
 document.getElementById('search-form').addEventListener('submit', function handleForm(event) {
     event.preventDefault();
-})
-
-// add event to search bar
-document.getElementById('search-submit').addEventListener("click", () => {
-    let text = document.getElementById('search-input').value;
-    showCourses("python", text)
 })
